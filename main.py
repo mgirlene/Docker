593f98cc92dd8ebe62352fa56cb761973ec56529
mgirlene <mgirlene.silva0908@gmail.com>
2021-11-25 19:12:34 -0300

    função que adiciona um número para cada data e novo csv

--- main.py
+++ main.py
@@ -21,3 +21,14 @@ display(df_sum)
 
 plt.stackplot(df_sum['date'], df_sum['new_cases'], labels=['Novos Casos']) #grafico do dataframe com a soma de novos casos
 plt.legend(loc = 'upper left')
+
+df_sum['Dia']= "" #cria a coluna dia
+
+def datas(column):
+   for i in range(len(df_sum)): 
+      df_sum[column][i] = i+1  #vai preenchendo a coluna com um número que representa a quantidade de dia desde o inicio da pandemia
+      
+datas('Dia') #chama a função
+print(df_sum)
+
+df_sum.to_csv("d3.csv") #gera um csv com o dataframe df_sum
